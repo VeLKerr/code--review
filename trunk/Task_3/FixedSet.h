14ace8d23ae043c561026b3d76410f21fb5b42c2
V.Yacovlev@gmail.com <V.Yacovlev@gmail.com@da03ea87-9239-46d4-681c-1b075b2ee36e>
2014-11-27 13:09:43 +0000

    Code review first iteration

--- trunk/Task_3/FixedSet.h
+++ trunk/Task_3/FixedSet.h
@@ -1,11 +1,17 @@
-#ifndef FIXEDSET_H
+﻿#ifndef FIXEDSET_H
 #define FIXEDSET_H
 
 #include "HashFunction.h"
-
+ 
 /************************************************************************/
 /* This is a class for storing set of integer numbers                   */
 /************************************************************************/
+
+/*= 
+  1. См. общие замечания из HashFunction.h
+  2. Документация на public часть класса оформляется в стиле Doxygen,
+    а private -- можно и обычными комментариями. У тебя сделано наоборот
+*/
 class FixedSet {
 
 private:
@@ -22,8 +28,24 @@ private:
     HashFunction getHashCoefs(const int k_value, const int prime_module);
 
 public: //Here is the functions, given in task condition.
+    /*= 
+      Тут вполне разумно сделано разделение стадий конструкции и инициализации,
+      но конструктор, который создает не до конца доделанный объект, не должен быть доступен
+      для использования извне.
+      Лучше сделать так:
+
+      public:
+          static FixedSet create(const std::vector<int> &number);
+      private:
+          explicit FixedSet();
+          void initialize(const std::vector<int> &numbers);
+    */
     FixedSet();
     void initialize(const std::vector<int> &numbers);
+
+    /*= Этот комментарий избыточен, посколько описывает стандартную практику.
+      Кроме того, он не соответствует действительности :-)
+      (см. замечание в .cpp-файле) */
     //This function is const because it isn't changes object's fields.
     const bool contains(const int number);
 };
